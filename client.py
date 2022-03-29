d87934f3e18e337e5d9504aa379e6fa5434596ad
SaberSwordBoy <saberfilmsproductions@gmail.com>
2022-03-29 11:49:25 -0400

    Add config

--- client.py
+++ client.py
@@ -1,49 +1,83 @@
 import requests
+from rich import print
+import configparser
+import os 
 
-DEBUG = True # Print out all data instead of just response. 
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+
+DEBUG = config.getboolean("Client", "debug") # Print additional data if DEBUG = True
 
 # Urls
-BASE_URL = 'http://45.33.77.180/'
-PORT = 80 # Not needed currently
+BASE_URL = f"http://{config.get('Client', 'server_ip')}/"
+PORT = config.get('Client', 'server_port')
 GETRESP_URL = 'getresponse'
 
-exit_vars = ["exit", "quit"] # If the user types anything in this list, the program will quit. 
+if DEBUG:
+    print(BASE_URL, PORT, GETRESP_URL)
 
 # Banner
 print("-"*50)
-print("\t   ✨ Welcome to Cassandra! ✨")
+print("[orchid]\t   ✨ Welcome to Cassandra! ✨[/orchid]")
 print("-"*50)
-print("Type a message and hit enter. Your message gets sent to the Cassie servers and processed using AI.\n"\
-      "The first message might take a few seconds longer than the rest.\n\n")
+print("[grey70]Type a message and hit enter. Your message gets sent to the Cassie server and processed using AI.\n"\
+      "The first message might take a few seconds longer than the rest.\nType 'help' for Cassie help, and '!help' for client help\n\n[/grey70]")
 
 print("Enter text to say to Cassandra")
 
-# Functions
+# Client functions
+def client_help():
+    os.system('cls')
+    os.system('clear')
+    print("-"*50)
+    print("\t\tCLIENT HELP")
+    print("-"*50)
+
+# Assistant Functions
 def assistant_help():
     print("-"*50)
     print("\t\tHELP")
     print("-"*50)
 
 def create_note():
-    # do stuff
-    print("Create a new note")
+    print("-"*50)
+    print("Create a note")
+    print("-"*50)
+
+def send_email():
+    print("-"*50)
+    print("Send Email")
+    print("-"*50)
 
 # Main Loop
 while True:
-    input_text = input(">>> ")
+    print("[purple blink]>>> [/purple blink]", end='')
+    input_text = input()
     
-    if input_text in exit_vars:
+    if input_text == '!exit': # exit
         print("Exiting!")
         break
+    elif input_text == '!clear' or input_text == '!cls': # clear the screen
+        os.system('cls')
+        os.system('clear')
+        continue
+    elif input_text == '!help': # call the help function
+        client_help()
+        continue
     
-    resp = requests.post(BASE_URL + GETRESP_URL, data={"input": input_text}).json()
+    try: resp = requests.post(BASE_URL + GETRESP_URL, data={"input": input_text}).json() # Request the URL and save the JSON
+    except ConnectionError: resp = {'data': {'response': '[red]UNABLE TO CONNECT TO SERVER[/red]'}}
+    except requests.exceptions.ConnectionError: resp = {'data': {'response': '[red]UNABLE TO CONNECT TO SERVER[/red]'}} 
+        
     resp_data = resp['data']
-
-    if resp_data.get('func'):
-        exec(f"{resp_data['func']}()")
-
-    if DEBUG:
+    
+    if resp_data.get('func'): # check if there is a function along with response
+        exec(f"{resp_data['func']}()")  # execute function given to us by server
+        
+    elif DEBUG:
         print(resp_data)
+
     else:
         print("You: " + input_text)
         print("Cassie: " + resp_data['response'])
+
